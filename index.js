94325787510d0ad62b27d802411182e2c7914148
Steve Terpe <srterpe@gmail.com>
2016-04-07 11:09:59 -0700

    Fix require statement typo

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-var quux = require('gx/ipfs/QmQV3Sz1mcaJmeP5yrqjVXBE2R2swNmyhxCs6s7jkfLPYB/quux.js')
+var quux = require('gx/ipfs/QmQV3Sz1mcaJmeP5yrqjVXBE2R2swNmyhxCs6s7jkfLPYB/gx-quux.js')
 
 module.exports = [ 'foo', quux ]
